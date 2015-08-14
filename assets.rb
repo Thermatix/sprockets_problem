71a1ad886aafe9490e002ee024671359149986af
Thermatix <mbeckerwork@gmail.com>
2015-08-14 13:54:17 +0100

    cleans up asset code

--- assets.rb
+++ assets.rb
@@ -12,11 +12,33 @@ module Assets
 	class << self
 		attr_reader :get_asset
 		attr_accessor :opal_config
+
+		def defaults
+			{
+				root: Dir.pwd,
+				asset_folder: 'assets',
+				asset_folders: %w{javascripts stylesheets fonts images},
+				assets: -> { 
+					Sprockets::Environment.new do |env|
+						asset_root = "#{settings.root}/#{settings.asset_folder}"
+						#register opal with sprockets
+						env.register_engine '.orb', Opal::Processor					
+						Opal.paths.each {|p|env.append_path p}
+						
+						#set up asset root folder
+						env.append_path asset_root
+						#set up compressers and pre-proccessors
+						env.js_compressor = YUI::JavaScriptCompressor.new
+						env.css_compressor = :sass
+					end
+				}
+			}
+		end
 	end
 
 	@get_asset = Proc.new do |file_name,extention,asset_type=nil|
 		content_type mime_types[extention]
-		path = file_name + extention#[asset_type,file_name + extention].compact.join('/')
+		path = [asset_type,"#{file_name}#{extention}"].compact.join('/')
 		env_sprockets = request.env.dup
 		env_sprockets['PATH_INFO'] = path
 		puts path
@@ -35,51 +57,32 @@ module Assets
 
 	def self.included base
 		base.class_exec(self) do |assets|
-			# set_default :root, Dir.pwd
-			# set_default :asset_folder, 'assets'
 
-			unless ENV['RACK_ENV'] == 'production'
-
+			if ENV['RACK_ENV'] == 'production'
+				Opal::Processor.source_map_enabled = false
+			else
+				Opal::Processor.source_map_enabled = false
 			end
-			asset_types = nil
-			asset_root = nil
+
+			asset_root = "#{settings.root}/#{settings.asset_folder}"
 			configure do
-
-				set :assets, Sprockets::Environment.new { |env|
-					env.register_engine '.orb', Opal::Processor
-
-					# ##set up opal paths
-					Opal.paths.each {|p|env.append_path p}
-
-					##set up "traditional" assets
-					# env.append_path settings.root 
-					asset_root = "#{settings.root}/#{settings.asset_folder}"
-					env.append_path asset_root
-					(asset_types = %w{javascripts stylesheets fonts images}).each do |asset_folder|
-						env.append_path "#{asset_root}/*"
-					end
-
-					#set up compressers and pre-proccessors
-					env.js_compressor = YUI::JavaScriptCompressor.new
-					env.css_compressor = :sass
-				}
-				::Sass.load_paths << asset_root
+				assets.defaults.each do |key,to_value|
+					set key, ( to_value.respond_to?(:call) ? self.instance_exec(&to_value) : to_value) unless settings.respond_to? key
+				end
+				::Sass.load_paths << "#{asset_root}/stylesheets"
 			end
-			Opal::Processor.source_map_enabled = false
+
+			
 			##add asset routes for asset_folder folder
 			
 			get %r{/#{settings.asset_folder}/(.*)(\W.*)$} do
-				s =	self.instance_exec(*params[:captures],&assets.get_asset)
-				puts s unless params[:captures].last == ".js"
-				s
+				self.instance_exec(*params[:captures],&assets.get_asset)
 			end
 
 			#add asset types
-			asset_types.each do |asset_type|
+			settings.asset_folders.each do |asset_type|
 				get %r{/#{settings.asset_folder}/#{asset_type}/(.*)(\W.*)$} do
-					s = self.instance_exec(*params[:captures],asset_type,&assets.get_asset)
-					puts s
-					s
+					self.instance_exec(*params[:captures],asset_type,&assets.get_asset)
 				end
 			end
 		end
